c28e575be6ec31cb519d25e320b5554fcddda346
nfx8 <ben@possiplex.com>
2013-04-19 14:00:47 +0200

    .

--- tests/PROJECT_tests.py
+++ tests/PROJECT_tests.py
@@ -1,11 +1,13 @@
 from nose.tools import *
-import PROJECT
+import PROJECT.foo as foo
 
 def setup():
     print "SETUP!"
 
-def teardown():
+def teardown():    
     print "TEAR DOWN!"
 
 def test_basic():
     print "I RAN!"
+    assert foo.add(1,1) == 2
+    
